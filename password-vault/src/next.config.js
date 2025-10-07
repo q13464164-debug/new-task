6b85ade4231a04ed9fe90cd5b8dfa9ee3f173379
Mandar <mandarjoshi1045@gmail.com>
2025-10-07 19:32:55 +0530

    now

--- password-vault/src/next.config.js
+++
@@ -1,9 +0,0 @@
-console.log("next.config.js: Loading config");
-
-const nextConfig = {
-  /* config options here */
-};
-
-console.log("next.config.js: Config loaded");
-
-module.exports = nextConfig;
